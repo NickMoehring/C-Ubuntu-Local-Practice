5ea85676bd3d670606c09fbd9335a7b533efdbc5
nick <nicholaswmoehring@gmail.com>
2018-07-31 12:32:37 -0500

    Added loop asking to play again, added loop for guessing the number, added high or low guess couts to user, adjusted print spacing with \n and endl

--- guessingGame.cpp
+++ guessingGame.cpp
@@ -1,13 +1,13 @@
 /*
  * [X] Intro program and prompt user for high and low bounds bounds
- * [] gen rand int
- * [] prompt for guess
+ * [X] gen rand int
+ * [X] prompt for guess
  * [] save guess and num guesses
- * [] tell user if high or low guess
- * [] loop guessing untill correct
+ * [X] tell user if high or low guess
+ * [X] loop guessing untill correct
  * [] print number of guesses ask if they want guess history
  * [] if y display rand num, then guesses with guess number below
- * [] loop guessing untill user prompts n
+ * [X] loop guessing untill user prompts n
  */
 
 #include <iostream>
@@ -18,16 +18,39 @@ int main(void)
 	int highBound;
 	int lowBound;
 	int ans;
+	int guess;
+	char prompt;
 
-	std::cout << "Hello, this is a number guessing game. What two integers would you like to guess between? (low then high, sperate with a space and hit enter)\n";
+	std::cout << "\nHello, this is a number guessing game.";
 
-	std::cin >> lowBound;
-	std::cin >> highBound;
+	do {
+		std::cout << "\nWhat two integers would you like to guess between?\n";
+		std::cout << "(low then high, sperate with a space and hit enter)\n";
+		std::cin >> lowBound;
+		std::cin >> highBound;
 
-	ans = rand() % (highBound - lowBound) + lowBound; // generates a rand num in bounds
+		ans = rand() % (highBound - lowBound) + lowBound; // generates a rand num in bounds
 
-	/*TESTING*/std::cout << "\nlowbound: " << lowBound << "       highbound: " << highBound << "        rand num: " << ans << std::endl;
+		/*TESTING*/std::cout << "\nlowbound: " << lowBound << "       highbound: " << highBound << "        rand num: " << ans << std::endl;
 
+		std::cout << "Please enter a guess in integer form: ";
+
+		do {
+			std::cin >> guess;
+
+			if (guess < ans)
+				std::cout << "Thats too low! Make another guess: ";
+			else if (guess > ans)
+				std::cout << "Thats too high! Make another guess: ";
+		}
+		while (guess != ans);
+
+		std::cout << "Congratulations, it was " << ans << "! Would you like to play again? (y/n): ";
+		std::cin >> prompt;
+	}
+	while (prompt == 'Y' || prompt == 'y');
+
+	std::cout << "Goodbye." << std::endl;
 
 	return 0;
 }
