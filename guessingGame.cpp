8eca6fb012dd273a449238c1270308739ebea133
nick <nicholaswmoehring@gmail.com>
2018-07-31 13:31:41 -0500

    Now displays number of guesses, added option for user to display guess history, saved guesses in vector object

--- guessingGame.cpp
+++ guessingGame.cpp
@@ -5,16 +5,18 @@
  * [X] Intro program and prompt user for high and low bounds bounds
  * [X] gen rand int
  * [X] prompt for guess
- * [] save guess and num guesses
+ * [X] save guess and num guesses
  * [X] tell user if high or low guess
  * [X] loop guessing untill correct
- * [] print number of guesses ask if they want guess history
- * [] if y display rand num, then guesses with guess number below
+ * [X] print number of guesses ask if they want guess history
+ * [X] if y display rand num, then guesses with guess number below
  * [X] loop guessing untill user prompts n
  */
 
-#include <iostream>
-#include <cstdlib>
+#include <iostream> // cin cout
+#include <cstdlib> // rand()
+#include <vector> // vector class
+#include <iomanip> // setw()
 
 int main(void)
 {
@@ -27,8 +29,10 @@ int main(void)
 	std::cout << "\nHello, this is a number guessing game.";
 
 	do {
-		std::cout << "\nWhat two integers would you like to guess between?\n";
-		std::cout << "(low then high, sperate with a space and hit enter)\n";
+		char histPrompt;
+		std::vector<int> guesses;
+
+		std::cout << "\nWhat two integers would you like to guess between? (seperate them with a space and hit enter)\n";
 		std::cin >> lowBound;
 		std::cin >> highBound;
 
@@ -38,14 +42,15 @@ int main(void)
 			lowBound = lowBound - highBound;
 		}
 
-		ans = rand() % (highBound - lowBound) + lowBound; // generates a rand num in bounds
+		ans = rand() % (highBound - lowBound) + lowBound; // generates a rand int in bounds
 
-		/*TESTING*/std::cout << "\nlowbound: " << lowBound << "       highbound: " << highBound << "        rand num: " << ans << std::endl;
+		///*TESTING*/std::cout << "\nlowbound: " << lowBound << "       highbound: " << highBound << "        rand num: " << ans << std::endl;
 
 		std::cout << "Please enter a guess in integer form: ";
 
 		do {
 			std::cin >> guess;
+			guesses.push_back(guess);
 
 			if (guess < ans)
 				std::cout << "Thats too low! Make another guess: ";
@@ -54,7 +59,24 @@ int main(void)
 		}
 		while (guess != ans);
 
-		std::cout << "Congratulations, it was " << ans << "! Would you like to play again? (y/n): ";
+		std::cout << "Congratulations, it was " << ans << ", and it only took " << guesses.size() << " guesses!\n";
+
+		std::cout << "Would you like to see your guess history? (y/n): ";
+		std::cin >> histPrompt;
+		if (histPrompt == 'y' || histPrompt == 'Y') {
+
+			std::cout << "Guess:      | ";
+			for (unsigned int i = 0; i < guesses.size(); i ++)
+				std::cout << std::setw(6) << guesses[i] << " | ";
+
+			std::cout << "\n";
+
+			std::cout << "Num. Tries: | ";
+			for (unsigned int j = 1; j <= guesses.size(); j++)
+				std::cout << std::setw(6) << j << " | ";
+		}
+
+		std::cout << "\nWould you like to play again? (y/n): ";
 		std::cin >> prompt;
 	}
 	while (prompt == 'Y' || prompt == 'y');
