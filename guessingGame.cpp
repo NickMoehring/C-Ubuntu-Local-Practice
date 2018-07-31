fa776ca294a8a15f19b9404016e2b209eba27ed4
nick <nicholaswmoehring@gmail.com>
2018-07-31 11:31:57 -0500

    Added int "ans" and generated a random number in the inputed bounds, included cstdlb for rand()

--- guessingGame.cpp
+++ guessingGame.cpp
@@ -1,5 +1,5 @@
 /*
- * [] Intro program and prompt user for high and low bounds bounds
+ * [X] Intro program and prompt user for high and low bounds bounds
  * [] gen rand int
  * [] prompt for guess
  * [] save guess and num guesses
@@ -11,17 +11,23 @@
  */
 
 #include <iostream>
+#include <cstdlib>
 
 int main(void)
 {
-//	int highBound;
+	int highBound;
 	int lowBound;
+	int ans;
 
 	std::cout << "Hello, this is a number guessing game. What two integers would you like to guess between? (low then high, sperate with a space and hit enter)\n";
 
 	std::cin >> lowBound;
 	std::cin >> highBound;
 
-	/*TESTING*/std::cout << "\nlowbound: " << lowBound << "       highbound: " << highBound << std::endl;
+	ans = rand() % (highBound - lowBound) + lowBound; // generates a rand num in bounds
+
+	/*TESTING*/std::cout << "\nlowbound: " << lowBound << "       highbound: " << highBound << "        rand num: " << ans << std::endl;
+
+
 	return 0;
 }
